f6f69f084fd5fb385de617e2553633af5ae64287
Mohsen Ahmadi <mohsen@ahmadi.pm>
2022-05-16 12:17:14 +0200

    add some code for first commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # first_js_project
-this is my first JavaScript project on git 
+
+this is my first JavaScript project on git
+
+## Change List
+
+- First Change:
---
+++ first_page.js
@@ -0,0 +1,8 @@
+document.write('Hello World!');
+console.log("I'm learning JavaScript in first day");
+var a = 5;
+var b = 7;
+var c = a + b;
+document.write(c.toString());
+var first_name = 'Mohsen';
+var last_name = 'Ahmadi';
